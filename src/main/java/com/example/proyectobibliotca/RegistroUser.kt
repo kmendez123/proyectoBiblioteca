05ff9df2a3c147e3d1c33f7c8f6b5257057b6f11
kmendez123 <karla.mendez.est@tecazuay.edu.ec>
2024-07-11 19:11:16 -0500

    elimino clase y agrego nuevo

--- src/main/java/com/example/proyectobibliotca/RegistroUser.kt
+++ src/main/java/com/example/proyectobibliotca/RegistroUser.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class prueba : AppCompatActivity() {
+class RegistroUser : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_prueba)
+        setContentView(R.layout.activity_registro_user)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
